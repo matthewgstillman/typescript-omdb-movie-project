1b509369f803b6d6252de72e303da4770773f5bc
matthewgstillman <matthewgstillman@gmail.com>
2022-04-19 12:47:08 -0600

    first commit

---
+++ src/components/APIComponent.tsx
@@ -0,0 +1,137 @@
+import React, {useState, FC} from 'react'
+import { Form, Button, Container } from "react-bootstrap";
+import { useForm, SubmitHandler } from "react-hook-form";
+import ArticleComponent from './ArticleComponent';
+
+export interface Movie{
+    Title: string;
+    Year: string;
+    Released: string;
+    Runtime: string;
+    Genre: string;
+    Director: string;
+    Writer: string;
+    Actors: string;
+    Plot: string;
+    Language: string;
+    Awards: string;
+    Poster: string;
+    Metascore: string;
+    imdbRating: string;
+    imdbID: string;
+    Type: string;
+    DVD: string;
+    BoxOffice: string;
+    Production: string;
+    Website: string;
+    Response: boolean;
+}
+
+interface FormValues{
+    movieTitle: string;
+}
+const APIComponent: FC = () => {
+  const [movieTitle, setMovieTitle ] = useState<string | "">("King+Richard")  
+  const [movieData, setMovieData] = useState<Movie | null>(null);
+  const [movieDataSubmitted, setMovieDatSubmitted] = useState<boolean>(false);
+
+  const { register, handleSubmit } = useForm<FormValues>();
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    setMovieTitle(data.movieTitle);
+    getMovie(data.movieTitle);
+    setMovieDatSubmitted(true);
+  };
+
+
+  const getMovie = (title: string) => {
+    fetch(
+        `http://www.omdbapi.com/?t=${title}&apikey=${process.env.REACT_APP_API_KEY}&plot=full`
+    )
+    .then((response) => response.json())
+    .then((data) => setMovieData({
+        Title: data.Title,
+        Year: data.Year,
+        Released: data.Released,
+        Runtime: data.Runtime,
+        Genre: data.Genre,
+        Director: data.Director,
+        Writer: data.Writer,
+        Actors: data.Actors,
+        Plot: data.Plot,
+        Language: data.Language,
+        Awards: data.Awards,
+        Poster: data.Poster,
+        Metascore: data.Metascore,
+        imdbRating: data.imdbRating,
+        imdbID: data.imdbID,
+        Type: data.Type,
+        DVD: data.DVD,
+        BoxOffice: data.BoxOffice,
+        Production: data.Production,
+        Website: data.Website,
+        Response: data.Response,
+    }))
+  }
+
+  console.log(movieData);
+
+  return (
+    <div>
+       <Container fluid className='mainContainer'>
+        <Form data-testid="mainForm" onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group className="mb-3" controlId="topic">
+              <Form.Label data-testid="formLabel">
+                Enter a Movie
+              </Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Movie Title"
+                {...register("movieTitle")}
+              />
+            </Form.Group>
+            <Button onClick={() => getMovie} data-testid="submitButton" variant="primary" type="submit">
+            Get Movie Data
+            </Button>
+          </Form>
+          <div>
+              {movieData !== null && movieData.Title!== undefined?
+                <ArticleComponent
+                Title={movieData.Title}
+                Year={movieData.Year}
+                Released={movieData.Released}
+                Runtime={movieData.Runtime}
+                Genre={movieData.Genre}
+                Director={movieData.Director}
+                Writer={movieData.Writer}
+                Actors={movieData.Actors}
+                Plot={movieData.Plot}
+                Language={movieData.Language}
+                Awards={movieData.Awards}
+                Poster={movieData.Poster}
+                Metascore={movieData.Metascore}
+                imdbRating={movieData.imdbRating}
+                imdbID={movieData.imdbID}
+                Type={movieData.Type}
+                DVD={movieData.DVD}
+                BoxOffice={movieData.BoxOffice}
+                Production={movieData.Production}
+                Website={movieData.Website}
+                Response={movieData.Response}
+             />
+              : <div>
+                  <br />
+                  <h4 className='errorMessage'>No movie was found with this title - please check the spelling or submit another movie.</h4>
+              </div>
+              }
+            {movieData !== null &&  movieData.Title ===  undefined?
+                <h4 className='errorMessage'>No movie was found with this title - please check the spelling or submit another movie.</h4>
+                :<></>
+            }
+          </div>
+          <br></br>
+          </Container>
+    </div>
+  )
+}
+
+export default APIComponent
---
+++ src/components/ArticleComponent.tsx
@@ -0,0 +1,78 @@
+import React, {FC} from 'react'
+import Image from 'react-bootstrap/Image'
+
+interface MovieArticle{
+    Title: string;
+    Year: string;
+    Released: string;
+    Runtime: string;
+    Genre: string;
+    Director: string;
+    Writer: string;
+    Actors: string;
+    Plot: string;
+    Language: string;
+    Awards: string;
+    Poster: string;
+    Metascore: string;
+    imdbRating: string;
+    imdbID: string;
+    Type: string;
+    DVD: string;
+    BoxOffice: string;
+    Production: string;
+    Website: string;
+    Response: boolean;
+}
+
+const ArticleComponent: FC<MovieArticle> = (
+    {
+    Title,
+    Year,
+    Released,
+    Runtime,
+    Genre,
+    Director,
+    Writer,
+    Actors,
+    Plot,
+    Language,
+    Awards,
+    Poster,
+    Metascore,
+    imdbRating,
+    imdbID,
+    Type,
+    DVD,
+    BoxOffice,
+    Production,
+    Website,
+    Response,
+    }
+) => {
+  return (
+    <div>
+        <br />
+        <div className="articleHeader">
+        <h2>{Title}</h2>
+        <h6>{Year} - {Runtime}</h6>
+        </div>
+        <Image className='moviePoster' src={Poster} fluid/>
+        <h5 className="movieYearAndRunTimeHeader">{Year} - {Runtime}</h5>
+        <br />
+        <p className="plotParagraph">{Plot}</p>
+        <br />
+        <div className="movieInfo">
+            <h5>Genre: <b>{Genre}</b></h5>
+            <h5>Director: <b>{Director}</b></h5>
+            <h5>Writer(s): <b>{Writer}</b></h5>
+            <h5>Starring: <b>{Actors}</b></h5>
+            <h5>IMDB Rating: <b>{imdbRating}/10</b></h5>
+            <h5>Metascore: <b>{Metascore}/100</b></h5>
+        </div>
+        <a href={`https://www.imdb.com/title/${imdbID}`}>More info on IMDB</a>
+    </div>
+  )
+}
+
+export default ArticleComponent;
