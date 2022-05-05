26281f01963fa1dcfc8fb5f58f5a69631de4c1d3
matthewgstillman <matthewgstillman@gmail.com>
2022-05-04 18:03:20 -0600

    Latest formatting changes

--- src/components/ArticleComponent.tsx
+++ src/components/ArticleComponent.tsx
@@ -35,16 +35,18 @@ const ArticleComponent: FC<MovieArticle> = (
         </div>
         <Image className='moviePoster' src={Poster} alt="movie poster image" data-testid="moviePosterImage" fluid/>
         <div className="movieCard" data-testid="movieCard">
-            <p className="plotParagraph" data-testid="plotParagraph">{Plot}</p>
-            <h6 data-testid="genreHeader"><b>Genre:</b> {Genre}</h6>
-            <h6 data-testid="directorHeader"><b>Director:</b> {Director}</h6>
-            <h6 data-testid="writerHeader"><b>Writer(s):</b> {Writer}</h6>
-            <h6 data-testid="actorHeader"><b>Starring:</b> {Actors}</h6>
-            <h6 data-testid="releasedHeader"><b>Released:</b> {Released}</h6>
-            <h6 data-testid="imdbRatingHeader"><b>IMDB Rating:</b> {imdbRating}/10</h6>
-            <h6 data-testid="metascoreHeader"><b>Metascore:</b> {Metascore}/100</h6>
-            <h6 data-testid="totalBoxOfficeHeader"><b>Total Box Office Revenue</b>: {BoxOffice}</h6>
-            <a  data-testid="moreInfoLink" className="moreInfoLink" href={`https://www.imdb.com/title/${imdbID}`}>More info on IMDB</a>
+           <div className="movieContainerData">
+           <p className="plotParagraph" data-testid="plotParagraph">{Plot}</p>
+              <h6 data-testid="genreHeader"><b>Genre:</b> {Genre}</h6>
+              <h6 data-testid="directorHeader"><b>Director:</b> {Director}</h6>
+              <h6 data-testid="writerHeader"><b>Writer(s):</b> {Writer}</h6>
+              <h6 data-testid="actorHeader"><b>Starring:</b> {Actors}</h6>
+              <h6 data-testid="releasedHeader"><b>Released:</b> {Released}</h6>
+              <h6 data-testid="imdbRatingHeader"><b>IMDB Rating:</b> {imdbRating}/10</h6>
+              <h6 data-testid="metascoreHeader"><b>Metascore:</b> {Metascore}/100</h6>
+              <h6 data-testid="totalBoxOfficeHeader"><b>Total Box Office Revenue</b>: {BoxOffice}</h6>
+              <a  data-testid="moreInfoLink" className="moreInfoLink" href={`https://www.imdb.com/title/${imdbID}`}>More info on IMDB</a>
+           </div>
         </div>
         <br />
     </div>
